513c1945affa3674bb4303a298e7bf6f3e36cb12
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-02-10 14:03:57 -0300

    alterando banco de dados para produção

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const etapasRouter = require('./routes/etapas');
 const orcamentosRouter = require('./routes/orcamentos.js');
 
 const app = express();
-const mongodb = 'mongodb+srv://lalves86:p3SXGK6PBUHCGuBL@cluster0-sammj.mongodb.net/lu-teperino-arq?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true';
+const mongodb = 'mongodb+srv://lalves86:p3SXGK6PBUHCGuBL@cluster0-sammj.mongodb.net/lu-teperino-arq-production?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true';
 
 mongoose.connect(mongodb, {
   useNewUrlParser: true,
